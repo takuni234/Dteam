13138c50f79362ba139465efce7df254a9d95394
taku1027 <zeal18300053@c-league.jp>
2020-03-10 14:58:53 +0900

    読み込み

--- ゲーム開発/FullSample100/GameSources/Scene.cpp
+++ ゲーム開発/FullSample100/GameSources/Scene.cpp
@@ -12,12 +12,30 @@ namespace basecross{
 	//--------------------------------------------------------------------------------------
 	///	ゲームシーン
 	//--------------------------------------------------------------------------------------
+	void Scene::CreateResourses() {
+		wstring dataDir;
+		//サンプルのためアセットディレクトリを取得
+		App::GetApp()->GetAssetsDirectory(dataDir);
+		//各ゲームは以下のようにデータディレクトリを取得すべき
+		//App::GetApp()->GetDataDirectory(dataDir);
+		wstring strTexture = dataDir + L"trace.png";
+		App::GetApp()->RegisterTexture(L"TRACE_TX", strTexture);
+		strTexture = dataDir + L"sky.jpg";
+		App::GetApp()->RegisterTexture(L"SKY_TX", strTexture);
+		strTexture = dataDir + L"wall.jpg";
+		App::GetApp()->RegisterTexture(L"WALL_TX", strTexture);
+
+	}
+	
 	void Scene::OnCreate(){
 		try {
 			//クリアする色を設定
 			Col4 Col;
 			Col.set(31.0f / 255.0f, 30.0f / 255.0f, 71.0f / 255.0f, 255.0f / 255.0f);
 			SetClearColor(Col);
+
+			CreateResourses();
+
 			//自分自身にイベントを送る
 			//これにより各ステージやオブジェクトがCreate時にシーンにアクセスできる
 			PostEvent(0.0f, GetThis<ObjectInterface>(), GetThis<Scene>(), L"ToGameStage");
