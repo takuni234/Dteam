ff07c6afaec152b79fb8acb764482a382212c112
human20002 <zeal19300005@c-league.jp>
2020-04-14 09:11:05 +0900

    マグマのランダム生成

--- ゲーム開発/FullSample100/GameSources/Character.cpp
+++ ゲーム開発/FullSample100/GameSources/Character.cpp
@@ -5,7 +5,6 @@
 
 #include "stdafx.h"
 #include "Project.h"
-
 namespace basecross{
 	//--------------------------------------------------------------------------------------
 	//	class FixedBox : public GameObject;
@@ -45,6 +44,67 @@ namespace basecross{
 		ptrDraw->SetFogEnabled(true);
 		ptrDraw->SetOwnShadowActive(true);
 
+	}
+	//--------------------------------------------------------------------------------------
+//	class IncreaseBox : public GameObject;
+//--------------------------------------------------------------------------------------
+//構築と破棄
+
+	IncreaseBox::IncreaseBox(const shared_ptr<Stage>& StagePtr,
+		const Vec3& Scale,
+		const Vec3& Rotation,
+		const Vec3& Position
+	) :
+		GameObject(StagePtr),
+		m_Scale(Scale),
+		m_Rotation(Rotation),
+		m_Position(Position)
+	{
+	}
+
+	IncreaseBox::~IncreaseBox() {}
+
+	//初期化
+	void IncreaseBox::OnCreate() {
+		check = 0;
+		auto ptrTransform = GetComponent<Transform>();
+		ptrTransform->SetScale(m_Scale);
+		ptrTransform->SetRotation(m_Rotation);
+		ptrTransform->SetPosition(m_Position);
+		//OBB衝突j判定を付ける
+		auto ptrColl = AddComponent<CollisionObb>();
+		ptrColl->SetFixed(true);
+		//タグをつける
+		AddTag(L"FixedBox");
+		//影をつける（シャドウマップを描画する）
+		auto shadowPtr = AddComponent<Shadowmap>();
+		//影の形（メッシュ）を設定
+		shadowPtr->SetMeshResource(L"DEFAULT_CUBE");
+		auto ptrDraw = AddComponent<BcPNTStaticDraw>();
+		ptrDraw->SetMeshResource(L"DEFAULT_CUBE");
+		//ptrDraw->SetTextureResource(L"SKY_TX");
+		ptrDraw->SetFogEnabled(true);
+		ptrDraw->SetOwnShadowActive(true);
+		ptrDraw->SetDiffuse(Col4(1, 0, 0, 1));
+
+
+
+	}
+	int IncreaseBox::checkkansu() {
+		return check;
+	}
+	void IncreaseBox::OnCollisionEnter(shared_ptr<GameObject>& Other) {
+		auto ptrDraw = GetComponent<BcPNTStaticDraw>();
+		//if (Other->FindTag(L"Player")) {
+			//TestObjectタグを持った相手と衝突した
+			ptrDraw->SetDiffuse(Col4(0, 1, 0, 1));
+			ptrDraw->SetTextureResource(L"SKY_TX");
+			DestroyGameObject();
+
+
+		//}
+
+		check = 1;
 	}
 
 	//--------------------------------------------------------------------------------------
@@ -125,5 +185,35 @@ namespace basecross{
 		ptrDraw->SetFogEnabled(true);
 		ptrDraw->SetOwnShadowActive(true);
 	}
+
+	//--------------------------------------------------------------------------------------
+	//	class IncreaseObject: public GameObject;
+	//--------------------------------------------------------------------------------------
+	IncreaseObject::IncreaseObject(const shared_ptr<Stage>& StagePtr):GameObject(StagePtr),count(0),x(0),z(0){}
+	IncreaseObject::~IncreaseObject(){}
+	void IncreaseObject::OnCreate(){
+
+	}
+	void IncreaseObject::OnUpdate() {
+		if (count != 15) {
+			//for (int i = 0; i < 5; i++) {
+			if (time == 60) {	
+				randomx = rand()%2;
+				randomz = rand() % 2;
+				if (!(randomx == 0 && randomz == 0)) {
+					x = x + randomx;
+					z = z + randomz;
+				}
+				GetStage()->AddGameObject<IncreaseBox>(Vec3(1, 1, 1), Vec3(0), Vec3(x, 0, z));
+				count++;
+				time = 0;
+			}	
+			time++;
+
+			//}
+		}
+
+	}
+
 }
 //end basecross
--- ゲーム開発/FullSample100/GameSources/Character.h
+++ ゲーム開発/FullSample100/GameSources/Character.h
@@ -5,6 +5,8 @@
 
 #pragma once
 #include "stdafx.h"
+#include <iostream>
+#include <stdio.h>
 
 namespace basecross{
 	//--------------------------------------------------------------------------------------
@@ -27,6 +29,50 @@ namespace basecross{
 		//ëÄçÏ
 	};
 
+	//--------------------------------------------------------------------------------------
+//	class IncreaseBox : public GameObject;
+//--------------------------------------------------------------------------------------
+	class IncreaseBox : public GameObject {
+		Vec3 m_Scale;
+		Vec3 m_Rotation;
+		Vec3 m_Position;
+
+	public:
+		int check;
+		//ç\ízÇ∆îjä¸
+	IncreaseBox(const shared_ptr<Stage>& StagePtr,
+			const Vec3& Scale,
+			const Vec3& Rotation,
+			const Vec3& Position
+		);
+		virtual int checkkansu();
+		void OnCollisionEnter(shared_ptr<GameObject>& Other);
+		
+
+		virtual ~IncreaseBox();
+		//èâä˙âª
+		virtual void OnCreate() override;
+		//ëÄçÏ
+	};
+	//--------------------------------------------------------------------------------------
+	//	class IncreaseObject: public GameObject;
+	//--------------------------------------------------------------------------------------
+
+	class IncreaseObject : public GameObject {
+		Vec3 m_Vec3;
+		int count;
+		int time;
+		int x;
+		int z;
+		int randomx;
+		int randomz;
+	public:
+		IncreaseObject(const shared_ptr<Stage>& StagePtr);
+		virtual ~IncreaseObject();
+		virtual void OnCreate() override;
+		virtual void OnUpdate() override;
+
+	};
 	//--------------------------------------------------------------------------------------
 	//	class FixedSphere : public GameObject;
 	//--------------------------------------------------------------------------------------
