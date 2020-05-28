99cfc8cc048ca7dee59c166d030ac737eed3da57
taku1027 <zeal18300053@c-league.jp>
2020-05-28 10:42:17 +0900

    ステセレの追加、ファイル整理

--- ゲーム開発/FullSample100/GameSources/GameStage.h
+++ ゲーム開発/FullSample100/GameSources/GameStage.h
@@ -60,7 +60,7 @@ namespace basecross {
 	public:
 
 		//\’z‚Æ”jŠü
-		GameStage() :Stage(), m_IsUpdate(true),m_TotalTime(1000) {}
+		GameStage() :Stage(), m_IsUpdate(true),m_TotalTime(App::GetApp()->GetScene<Scene>()->GetLimitTime()) {}
 		virtual ~GameStage() {}
 		//‰Šú‰»
 		virtual void OnCreate()override;
